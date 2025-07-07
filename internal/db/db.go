81f748fffa30f04659367fa57627b8633438d983
krystian <juszczyk-krystian@wp.pl>
2025-07-07 13:06:29 +0200

    Implement data loading and saving functions; add initial JSON files for users and tasks

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,1 +1,25 @@
 package db
+
+import (
+	"encoding/json"
+	"os"
+)
+
+const UserFile = "users.json"
+const TaskFile = "tasks.json"
+
+func LoadData(filename string, data interface{}) error {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(file, data)
+}
+
+func SaveData(filename string, data interface{}) error {
+	file, err := json.MarshalIndent(data, "", " ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(filename, file, 0644)
+}
